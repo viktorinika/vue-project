28a8b409f70e966f00b2a548a478d1217acec9b3
Viktoriia S <viksavrub@gmail.com>
2023-08-26 18:51:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld 
+        :msg="'Hello Header!'"
+      />
+    </div>
+  </header>
+
+  <main>
+  <Button :text="'submit'" />
+  
+  <Button :text="'dfgdf'" />
+  <Button :text="'item'" />
+  <Button :text="'456'" />
+  <Button :text="'2345cvb'" />
+  </main>
+</template>
---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <button class="button btn dfgfg">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 2px;
+}
+</style>
